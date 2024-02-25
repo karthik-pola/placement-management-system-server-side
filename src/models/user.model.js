06fbc87757e8c270ab7e7768b1d658e5d926d154
karthik-pola <karthikpola07@gmail.com>
2024-02-25 08:44:08 +0530

    add usermodel

---
+++ src/models/user.model.js
@@ -0,0 +1,125 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const userSchema = new Schema( 
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        avatar:{
+            type: String,
+        },
+        coverImage:{
+            type:String,
+        },
+        password: {
+            type: String,
+            required: [true , 'password is required']
+        },
+        refreshToken: {
+            type: String,
+        },
+        role:{
+            type: String,
+            required: true ,
+        },
+        personalEmail:{
+            type: String,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        ROLLnO:{
+            type: String,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        address:{
+            type: String,
+            lowercase: true,
+        },
+        gender:{
+            type:String,
+        },
+        course:{
+            type:String,
+        },
+        about:{
+            type:String,
+        },
+        department:{
+            type:String,
+        },
+        phoneNo:{
+            type:Number,
+        },
+        refreshToken:{
+            type:String,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+userSchema.pre("save",async function(next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = async function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const user = mongoose.model("User",userSchema);
