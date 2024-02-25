1aa050ad0cd55eb559e09b690b326546f1858496
karthik-pola <karthikpola07@gmail.com>
2024-02-25 22:50:26 +0530

    debuging the errors with the help of postman

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -27,8 +27,8 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/update-user-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-user-coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 
 
