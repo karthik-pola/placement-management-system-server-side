6cc7b3f6c8163e6b54b7f96238f4c42355d818ec
karthik-pola <karthikpola07@gmail.com>
2024-02-25 20:00:25 +0530

    login route updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logoutUser, registerUser } from "../controllers/user.controller.js";
+import { logoutUser, registerUser,loginUser } from "../controllers/user.controller.js";
 import  {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,7 +20,9 @@ router.route("/register").post(
     registerUser
     )
 
+router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+
 
 
 
